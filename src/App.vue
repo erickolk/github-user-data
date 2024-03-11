db52cd2c709dce5f6123ea81a7f1aed9f69024ff
erickolk <erickdynsouza@hotmail.com>
2024-03-11 14:37:29 -0300

    Feat: Projeto completo com vue para pegar dados dos usuários do Github

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+  data(){
+    return{
+      count: 0
+    }
+  }
+}
+
+</script>
+
+<template>
+  <button  v-on:click="count++">Contador: {{ count }}</button>
+</template>
+
+<style>
+#app{
+  display: grid;
+  place-content: center;
+  min-height: 100vh;
+}
+
+button{
+  font-size: 2rem;
+}
+</style>
